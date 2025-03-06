3052ddb9a2c4b5ee8146d5d4a8707386b91b1e8c
Diana Krainer <Diana.Krainer@ait.ac.at>
2025-03-06 17:30:57 +0100

    update main

--- abm/admm/main.jl
+++ abm/admm/main.jl
@@ -1,6 +1,6 @@
 using DataFrames: DataFrame
 import YAML
-
+using Random: seed!
 # NOTEs:
 # - `"julia.NumThreads": 16` in `settings.json`, or `-threads=16`, or ... (with any number of course)
 # - `Threads.nthreads()` to check the number of threads
@@ -12,7 +12,8 @@ cd(@__DIR__)
 include("src/ADMM.jl")
 
 # TODO: load proper data from a file
-T = 8 * 84  # = 4 * 168
+seed!(42)
+T = 1 * 168  # = 4 * 168
 data = DataFrame(Dict("demand" => 35.0 .+ 65.0 .* rand(T), "wind" => rand(T), "solar" => rand(T) .* rand(T)))
 
 admm = ADMM.setup("config/default.yaml", data)
@@ -21,8 +22,8 @@ ADMM.update_ρ!(admm)
 # ---- callbacks for the in-iteration updates
 
 function cb_agg(eq, λ_prev, sol)
-    derating = Dict(:thermal => 0.94, :wind => 0.32, :solar => 0.01, :storage => 0.6)  # TODO: from config
-
+    # derating = Dict(:thermal => 0.94, :wind => 0.32, :solar => 0.01, :storage => 0.6)  # TODO: from config
+    derating = Dict(:thermal => 1, :wind => 0.32, :solar => 0.01, :storage => 1)
     # Number of agents bidding into this "equation".
     n = length(sol)
 
@@ -47,7 +48,7 @@ function cb_agg(eq, λ_prev, sol)
         λ = min.(admm.cfg.eom.price_cap, λ)
     elseif eq == :cm
         # Oversupply is fine, but should not be requested.
-        E = min(0.0, E)
+        # E = min(0.0, E)
 
         # Prices can not be negative.
         λ = max(0.0, λ)
@@ -58,14 +59,14 @@ end
 
 function cb_update(eq::Symbol, agent::Symbol, λ, E)
     if eq == :cm
-        derating = Dict(:thermal => 0.94, :wind => 0.32, :solar => 0.01, :storage => 0.6)  # TODO: from config
-
+        # derating = Dict(:thermal => 0.94, :wind => 0.32, :solar => 0.01, :storage => 0.6)  # TODO: from config
+        derating = Dict(:thermal => 1, :wind => 0.32, :solar => 0.01, :storage =>1)
         if haskey(derating, agent)
             λ *= derating[agent]
             E /= derating[agent]
         end
     elseif eq == :eom
-        res_feedin_tariff = Dict(:wind => 15.0, :solar => 15.0)  # TODO: from config
+        res_feedin_tariff = Dict(:wind => 0.0, :solar => 0.0)  # TODO: from config
 
         if haskey(res_feedin_tariff, agent)
             λ = λ .+ res_feedin_tariff[agent]
@@ -79,7 +80,7 @@ end
 t_start = time()
 
 info = []
-for k in 1:1000
+for k in 1:2000
     status_codes = ADMM.solve!(admm; threaded=true)
     # TODO: actually check the status codes
 
